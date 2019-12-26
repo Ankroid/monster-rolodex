28a913042b3d5e7038f3bb21626b4a3485fc92da
Ankur Badola <ankur.badola@kony.com>
2019-12-26 22:54:09 +0530

    Updates

---
+++ precache-manifest.785a114edd68fb5c17b784e533d8eb1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293d6bb32974122ff438302dc48b2502",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "a87d33f3e21f0e38020e",
+    "url": "/monster-rolodex/static/css/main.fd12f377.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/monster-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a87d33f3e21f0e38020e",
+    "url": "/monster-rolodex/static/js/main.058f236e.chunk.js"
+  },
+  {
+    "revision": "cd391451e66ac7af83df",
+    "url": "/monster-rolodex/static/js/runtime-main.1356d2e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
